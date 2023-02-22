729c223091646561c78184a2a72ca45b0e233dec
Maschell <Maschell@gmx.de>
2023-02-22 16:15:20 +0100

    Fix connecting to a 3DS on Mii Maker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM wiiuenv/devkitppc:20221228
+FROM wiiuenv/devkitppc:20230218
 
-COPY --from=wiiuenv/wiiupluginsystem:20230126 /artifacts $DEVKITPRO
+COPY --from=wiiuenv/wiiupluginsystem:20230215 /artifacts $DEVKITPRO
 COPY --from=wiiuenv/libnotifications:20230126 /artifacts $DEVKITPRO
 COPY --from=wiiuenv/librpxloader:20220903 /artifacts $DEVKITPRO
 COPY --from=wiiuenv/libcurlwrapper:20230121 /artifacts $DEVKITPRO
--- src/utils/config.h
+++ src/utils/config.h
@@ -4,35 +4,36 @@
 #include <wups/config/WUPSConfigItemBoolean.h>
 #include <wups/storage.h>
 
+#define CAT_CONFIG                          "config"
+#define CAT_OTHER                           "other"
 
-#define CAT_CONFIG                "config"
-#define CAT_OTHER                 "other"
+#define USTEALTH_CONFIG_ID                  "ustealth"
+#define POWEROFFWARNING_CONFIG_ID           "SkipPowerOffWarning"
+#define FORCE_NDM_SUSPEND_SUCCESS_CONFIG_ID "forceNDMSuspendSuccess"
+#define ALLOW_ERROR_NOTIFICATIONS           "allowErrorNotifications"
 
-#define USTEALTH_CONFIG_ID        "ustealth"
-#define POWEROFFWARNING_CONFIG_ID "SkipPowerOffWarning"
 
+#define CONFIG_MENU_HINT_SHOWN_ID           "configMenuHintShown"
+#define LAST_UPDATE_HASH_ID                 "lastUpdateHash"
 
-#define CONFIG_MENU_HINT_SHOWN_ID "configMenuHintShown"
-#define LAST_UPDATE_HASH_ID       "lastUpdateHash"
-
-#define LOAD_BOOL_FROM_STORAGE(__cat, config_name, __variable__)                                                  \
-    if ((storageRes = WUPS_GetBool(__cat, config_name, &__variable__)) == WUPS_STORAGE_ERROR_NOT_FOUND) {         \
-        if (WUPS_StoreBool(__cat, config_name, __variable__) != WUPS_STORAGE_ERROR_SUCCESS) {                     \
-            DEBUG_FUNCTION_LINE_WARN("Failed to store bool");                                                     \
-        }                                                                                                         \
-    } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                        \
-        DEBUG_FUNCTION_LINE_WARN("Failed to get bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes); \
-    }                                                                                                             \
-    while (0)
-
-#define LOAD_STRING_FROM_STORAGE(__cat, config_name, __string, __string_length)                                         \
-    if ((storageRes = WUPS_GetString(__cat, config_name, __string, __string_length)) == WUPS_STORAGE_ERROR_NOT_FOUND) { \
-        if (WUPS_StoreString(__cat, config_name, __string) != WUPS_STORAGE_ERROR_SUCCESS) {                             \
-            DEBUG_FUNCTION_LINE_WARN("Failed to store string");                                                         \
+#define LOAD_BOOL_FROM_STORAGE(__cat, config_name, __variable__)                                                        \
+    if ((storageRes = WUPS_GetBool(__cat, config_name, &__variable__)) == WUPS_STORAGE_ERROR_NOT_FOUND) {               \
+        if (WUPS_StoreBool(__cat, config_name, __variable__) != WUPS_STORAGE_ERROR_SUCCESS) {                           \
+            DEBUG_FUNCTION_LINE_WARN("Failed to store bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes); \
         }                                                                                                               \
     } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                              \
         DEBUG_FUNCTION_LINE_WARN("Failed to get bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);       \
     }                                                                                                                   \
+    while (0)
+
+#define LOAD_STRING_FROM_STORAGE(__cat, config_name, __string, __string_length)                                           \
+    if ((storageRes = WUPS_GetString(__cat, config_name, __string, __string_length)) == WUPS_STORAGE_ERROR_NOT_FOUND) {   \
+        if (WUPS_StoreString(__cat, config_name, __string) != WUPS_STORAGE_ERROR_SUCCESS) {                               \
+            DEBUG_FUNCTION_LINE_WARN("Failed to store string %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes); \
+        }                                                                                                                 \
+    } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {                                                                \
+        DEBUG_FUNCTION_LINE_WARN("Failed to get bool %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);         \
+    }                                                                                                                     \
     while (0)
 
 #define PROCESS_BOOL_ITEM_CHANGED(cat, __config__name, __variable__)              \
@@ -49,3 +50,5 @@ extern bool gSkip4SecondOffStatusCheck;
 extern bool gConfigMenuHintShown;
 extern std::string gLastHash;
 extern bool gUpdateChecked;
+extern bool gForceNDMSuspendSuccess;
+extern bool gAllowErrorNotifications;
