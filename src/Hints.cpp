50807e4a600a9cd0cf588f2582083e9a14df520f
Maschell <Maschell@gmx.de>
2024-02-13 22:58:20 +0100

    Update Hints.cpp

--- src/Hints.cpp
+++ src/Hints.cpp
@@ -30,7 +30,7 @@ void ShowHints() {
         const char *tmp_file = "fs:/vol/external01/wiiu/write_lock";
         int fd               = -1;
         if ((fd = open(tmp_file, O_CREAT | O_TRUNC | O_RDWR)) < 0) {
-            DEBUG_FUNCTION_LINE_VERBOSE("SD Card mounted but not writable. %d , %s", errno, strerror(errno));
+            DEBUG_FUNCTION_LINE_ERR("SD Card mounted but not writable. %d , %s", errno, strerror(errno));
             NotificationModuleStatus err;
             NMColor red = {237, 28, 36, 255};
             NotificationModuleHandle outHandle;
